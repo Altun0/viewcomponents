54cf9a30cc11f315d6e75ab91f6f84065fc9d7d5
Altun0 <91114236+Altun0@users.noreply.github.com>
2022-01-10 22:39:12 +0400

    first commit

---
+++ Product.cs
@@ -0,0 +1,6 @@
+﻿namespace FrontToBack.ViewComponents
+{
+    internal class Product
+    {
+    }
+}
---
+++ ProductViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using FrontToBack.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly Context _context;
+        public ProductViewComponent(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task < IViewComponentResult> InvokeAsync()
+        {
+            List<Product> products = _context.Products
+                .Include(0=> p.Category)
+                .Take(8)
+                .ToList()
+
+                return View(await Task.FromResult(products));
+        }
+
+    }
+}
